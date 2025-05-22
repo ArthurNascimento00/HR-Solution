53b944480cfe0d4a18e7f311fabcd18ab5ea0884
ArthurNascimento00 <arthur.nascimento.senai404@gmail.com>
2025-05-22 16:49:32 -0300

    Add files via upload

--- HR Solution/script.js
+++ HR Solution/script.js
@@ -1,5 +1,20 @@
-document.getElementById("contact-form").onsubmit = function(event) {
-    event.preventDefault();
-    alert("Formulário enviado com sucesso!");
-    this.reset();
-};
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+  if (i < 0) index = images.length - 1;
+  else if (i >= images.length) index = 0;
+  else index = i;
+
+  slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+prevBtn.addEventListener('click', () => showSlide(index - 1));
+nextBtn.addEventListener('click', () => showSlide(index + 1));
+
+// Avanço automático a cada 6 segundos
+setInterval(() => showSlide(index + 1), 6000);
